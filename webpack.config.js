25a3ed8171602216246a2685d6e00c049797c10b
Sasoun Torossian <sasountorossian@gmail.com>
2020-11-20 18:45:06 +0000

    Moved position of .error-container in scss.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  // devtool: 'cheap-module-source-map',
+  mode: 'production',
+  devtool: 'cheap-module-source-map',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
